b1c3db8f6966a3788da3546736aeb6d58cff61b9
Your Name <arturoisraelperezvargas@MacBook-Pro-de-Arturo-2.local>
2018-03-22 02:21:17 -0600

    agencia

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+
+
+(function(){
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function(){
+       
+        /* var navegacion= document.getElementById("navegacion");
+       navegacion.style.backgroundColor="black";    aqui podemos cambiar los estilos con esta estructura*/
+
+       /*var navegacion=document.getElementsByClassName("navegacion");
+       console.log(navegacion[0]);  aqui accedemos a las clases con el nombre navegacion y especificamos en la que esta en primer lugar */
+
+      /* var enlaces=document.getElementsByTagName("a");
+          console.log(enlaces.length);
+        
+        for (var i = 0; i < enlaces.length; i++) {
+        	
+        	enlaces[i].setAttribute("target","_blank"); 
+        }
+
+        var enlacesSidebar=document.getElementById("sidebar").getElementsByTagName("a");
+
+        for (var i = 0; i < enlacesSidebar.length; i++) {
+        	enlacesSidebar[i].setAttribute("href","https://www.google.com.mx/");
+
+        }*/
+
+
+        	/*var enlaces=document.querySelectorAll("a"); /*seleccionamos todos los elementos a*/
+
+        	/*for (var i = 0; i < enlaces.length; i++) {
+        		
+        		enlaces[i].setAttribute("target","_blank");/*insertamos un target blank a todos los elementos*/
+        	/*}*/
+
+        	/*enlaces.id="holaaa" /* agregamos un id a navegacion */
+
+        	/*var sideBar=document.querySelector("#sidebar");
+        	var nuevoElemento=document.createElement("LI");
+        	var nuevoTexto=document.createTextNode("Hola mundo cruel");
+               nuevoElemento.appendChild(nuevoTexto);
+        	   sideBar.appendChild(nuevoElemento);*/
+
+
+        	   /*var sidebar=document.querySelector("aside");
+        	   var masVisitados=document.createElement("H2");
+        	   var textoVisitados=document.createTextNode("Post mas visitados.");
+                    masVisitados.appendChild(textoVisitados);
+        	   sidebar.insertBefore(masVisitados, sidebar.childNodes[0]);*/
+        	  
+
+       
+
+        
+  });
+
+})();
+
